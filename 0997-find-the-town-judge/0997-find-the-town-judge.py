0df944779315a4d2dc3a6dde5b53967a096ce0e0
Harsh Agarwal <155905327+Harsh741334@users.noreply.github.com>
2026-06-29 11:16:57 +0530

    Time: 10 ms (73.35%), Space: 22.8 MB (20.36%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,40 +1,11 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
 
-        if n == 1 :
-            return 1
-
-        if not trust:
-            return -1
-        judge = {} # A -- TRUST --> B 
-
-        tru = {} #  A  ---> TRUSTED BY --> B
-
+        trust_count = [0 for i in range(n)]
         for u,v in trust:
-            if u in judge and v in tru:
-                judge[u] = judge[u] + 1
-                tru[v] = tru[v] + 1
-            elif v in tru:
-                tru[v] = tru[v] + 1
-                judge[u] = 1
-        
-            elif u in judge:
-                judge[u] = judge[u] + 1
-                tru[v] = 1
-                        
-            else:
-                judge[u] = 1
-                tru[v]= 1
-
-        size = n-1
-
-        for i in range(1,n+1):
-            if i not in judge  and i in tru:
-                if tru[i] == size:
-                 return i
-
+            trust_count[u -1]-=1 # <--- in degree
+            trust_count[v -1]+=1  #<--- out degree
+        for i in range(n):
+            if trust_count[i]==(n-1):
+                return i+1
         return -1
-           
-           
-            
-        
